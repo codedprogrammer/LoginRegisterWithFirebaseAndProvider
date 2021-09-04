1c1589ee01b1e0310e3edebd5dcf8abadf1c3df3
Muhammad Abdullahi Nma <66219684+codedprogrammer@users.noreply.github.com>
2021-09-04 21:57:46 +0100

    firebase files for ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_register_with_firebase_and_provider/screens/sign_in.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
